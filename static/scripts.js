72d00cacf8453005ee4e7da744c2b4138ae799b4
Christopher Girvin <bugbounty.chrisg8691@gmail.com>
2020-08-19 05:32:51 -0700

    fixed submit button on find_entry page

--- static/scripts.js
+++ static/scripts.js
@@ -1,4 +1,20 @@
 function showPassword() {
-    const showBtn = document.getElementById('showPwBtn')
-    show.Btn.add
+    const pwDiv = document.getElementById("password");
+    if (pwDiv.style.display === "none") {
+        pwDiv.style.display = "block";
+    } else {
+        pwDiv.style.display = "none";
+    }
+
+    const button = document.getElementsByTagName("BUTTON")[0];
+    if (pwDiv.style.display === 'none') {
+        button.innerHTML = 'Show Password';
+    } else {
+        button.innerHTML = 'Hide Password';
+    }
+}
+
+function submit() {
+    const form = document.getElementById('serviceForm')
+    form.submit()
 }
